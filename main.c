05fb959c5cbf64d9b5010535b354eb062ef46772
eladsez <eladsez36@gmail.com>
2021-12-17 05:40:18 -0500

    almost done

--- main.c
+++ main.c
@@ -9,13 +9,20 @@ int main() {
 
   for (int i = 0; i < WORD; ++i){
     scanf("%c",&word[i]);
-    if (word[i] == ' ' || word[i] == '\t' || word[i] == '\n')
+    if (word[i] == ' ' || word[i] == '\t' || word[i] == '\n'){
+      word[i] = '\0';
       break;
+    }
   }
   for (int i = 0; i < TXT; ++i){
     scanf("%c",&text[i]);
-    if (text[i] == '~')
+    if (text[i] == '~'){
+      text[i + 1] = '\0';
       break;
+    }
   }
+
   AFunc(word, text);
+  printf("\n");
+  BFunc(word, text);
 }
--- string_parse.c
+++ string_parse.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #include "string_parse.h"
 #define TXT 1024
 #define WORD 30
 #define TRUE 1
+#define FALSE 0
 
 //return the geo value of *word until *(word + n) not include
 int geoValue(char* word, int n){
@@ -24,14 +26,16 @@ void validChack(char* text, int* start){
 }
 
 //print *word until *(word + n) not include
-void printCArr(char* ptr, int n){
+void printCArr(char* ptr, int n, int flag){
+  if (flag) printf("~");
   for (int i = 0; i < n; i++)
     printf("%c",ptr[i]);
-  printf("~");
+
 }
 
 void AFunc(char word[WORD], char text[TXT]){
-  char ans[TXT] = "";
+  printf("Gematria Sequences: ");
+  int flag = FALSE;
   int wordVal = geoValue(word, strlen(word));
   int start = 0;//include
   int end = 1; //not include
@@ -41,9 +45,10 @@ void AFunc(char word[WORD], char text[TXT]){
       break;
     if (currVal == wordVal){
       validChack(text,&start);
-      printCArr((text + start), end - start);
+      printCArr((text + start), end - start, flag);
       currVal -= geoValue(text + start, 1);
       ++start;
+      flag = TRUE;
     }
     if (currVal > wordVal){
       currVal -= geoValue(text + start, 1);
@@ -56,8 +61,76 @@ void AFunc(char word[WORD], char text[TXT]){
   }
 }
 
-void BFunc(char word[WORD], char text[TXT]){
+void makeAtbash(char word[WORD], char* atbash){
+  for (int i = 0; i < strlen(word); i++){
+    if (isalpha(word[i])){
 
+      if (isupper(word[i]))
+        *(atbash + i) = (char)(91 - ((int)word[i] - 64));
+
+      if (islower(word[i]))
+        *(atbash + i) = (char) (123 - ((int)word[i] - 96));
+    }
+    else *(atbash + i) = word[i];
+  }
+  *(atbash + strlen(word)) = '\0';
+}
+
+void reverse(char* word, int len){
+  char temp;
+  int i = 0;
+  int j = len - 1;
+  while(i < j){
+    temp = *(word + i);
+    *(word + i) = *(word + j);
+    *(word + j) = temp;
+    j--;
+    i++;
+  }
+}
+
+int atbashcmp(char *atbash, char *text, int end){
+  int iAtbash = 0;
+  int iText = 0;
+  while (iAtbash < strlen(atbash) && iText < end){
+    if (text[iText] == ' ') {
+      ++iText;
+      continue;
+    }
+    else if(text[iText] != atbash[iAtbash]) return FALSE;
+    ++iText;
+    ++iAtbash;
+  }
+  return TRUE;
+}
+
+void BFunc(char word[WORD], char text[TXT]){
+  printf("Atbash Sequences: ");
+  char *atbash = malloc(strlen(word) * sizeof(char) + 1);
+  char *reverseAtbash = malloc(strlen(word) * sizeof(char) + 1);
+
+  makeAtbash(word, atbash);
+  strcpy(reverseAtbash, atbash);
+  reverse(reverseAtbash, strlen(reverseAtbash));
+
+  int start = 0;
+  int end = strlen(word);
+  int flag = FALSE;
+  while (start < strlen(text) - 1 - strlen(word)){
+    while (text[end + 1] != '~'){
+      if(atbashcmp(atbash, text + start, end - start) || atbashcmp(reverseAtbash, text + start, end - start)){
+        printCArr((text + start), end - start, flag);
+        flag = TRUE;
+        break;
+      }
+      ++end;
+    }
+    ++start;
+    end = start + strlen(word);
+  }
+
+  free(reverseAtbash);
+  free(atbash);
 }
 
 void CFunc(char word[WORD], char text[TXT]){
